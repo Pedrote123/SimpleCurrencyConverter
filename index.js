d6b56e65bb7a088b69d824d3eb289e806cdbc8aa
Pedrote123 <[200~pedrocovetta123@gmail.com~>
2023-10-02 16:12:45 -0300

    Stylized

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     inpcrcy.onkeyup = ()=>{
         if (inpcrcy.value.length > 0){
             btn.disabled = false 
+            btn.setAttribute('id', 'avaible')
         } else{
             btn.disabled = true
+            if (btn.querySelector('#avaible')){
+                btn.removeAttribute('id', 'avaible')
+            }
         }
 
     }
@@ -25,11 +29,29 @@ document.addEventListener('DOMContentLoaded', ()=>{
             return response.json();
         })
         .then(data => {
-            var conversion = document.createElement('p');
-            conversion.innerHTML = `1 USD equals to ${data.rates[vlrinpt]} ${vlrinpt}`
-            var body = document.querySelector('body');
-            document.querySelector('.mainDiv').appendChild(conversion)
-            document.querySelector('.mainDiv').insertBefore(conversion, document.querySelector('#frm'))
+            if (document.querySelector('.conversion')){
+                if (!data.rates[vlrinpt]){
+                    document.querySelector('.conversion').innerHTML = "Introduced currency isn't valid"
+                } else{
+                    document.querySelector('.conversion').innerHTML = `1 USD equals to ${data.rates[vlrinpt]} ${vlrinpt}`
+                }
+            } else if (!document.querySelector('.conversion')){
+                if (!data.rates[vlrinpt]){
+                    var conversion = document.createElement('p');
+                    conversion.classList.add('conversion')
+                    conversion.innerHTML = "Introduced currency isn't valid"
+                    document.querySelector('.mainDiv').appendChild(conversion)
+                    document.querySelector('.mainDiv').insertBefore(conversion, document.querySelector('#frm'))
+                } else{
+                    var conversion = document.createElement('p');
+                    conversion.classList.add('conversion')
+                    conversion.innerHTML = `1 USD equals to ${data.rates[vlrinpt]} ${vlrinpt}`
+                    document.querySelector('.mainDiv').appendChild(conversion)
+                    document.querySelector('.mainDiv').insertBefore(conversion, document.querySelector('#frm'))
+                }
+
+            }
+            
         })
     })
 
